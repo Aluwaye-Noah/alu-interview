c9f39f6a9a86792783b4b8a3e097f57f26f4f0fa
root <a.yusuf1@alustudent.com>
2023-02-09 01:58:04 -0800

    correction

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-method that calculates the fewest no of opperations to get the result in exactly n H chara in a file
+method that calculates the fewest opperations to get the result in exactly n H chara in a file
 """
 
 
