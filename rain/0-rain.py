380e4cf987d773646da9de2d97792b4c7616788e
root <a.yusuf1@alustudent.com>
2023-03-23 06:59:03 -0700

    0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 
-"""
-This function calculates the amount of water retained after it rains between the walls.
-"""
+"""How many square units of water will be retained after it rains."""
+
 
 def rain(walls):
-    """
-    This function calculates the rainwater retained
-    """
+    """Function that calculates the rainwater retained."""
+
     if len(walls) == 0:
         return 0
-
-    left_wall, right_wall = 0, len(walls) - 1
-    left_wall_max, right_wall_max = walls[left_wall], walls[right_wall]
+    
+    bar_left, bar_right = 0, len(walls) - 1
+    bar_left_max, bar_right_max = walls[bar_left], walls[bar_right]
     amount_water = 0
 
-    while left_wall < right_wall:
-        if left_wall_max < right_wall_max:
-            left_wall += 1
-            left_wall_max = max(left_wall_max, walls[left_wall])
-            amount_water += left_wall_max - walls[left_wall]
+    while bar_left < bar_right:
+        if bar_left_max < bar_right_max:
+            bar_left += 1
+            bar_left_max = max(bar_left_max, walls[bar_left])
+            amount_water += bar_left_max - walls[bar_left]
         else:
-            right_wall -= 1
-            right_wall_max = max(right_wall_max, walls[right_wall])
-            amount_water += right_wall_max - walls[right_wall]
-
+            bar_right -= 1
+            bar_right_max = max(bar_right_max, walls[bar_right])
+            amount_water += bar_right_max - walls[bar_right]
+    
     return amount_water
